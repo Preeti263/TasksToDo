7e6296080db0f476fbe03dd5226aefba94574eee
preeti_pandey <t.preeti1234@gmail.com>
2022-04-02 10:58:38 +0100

    Adding Basic View and Task DB , Initial Migration, Basic Framework for Automapper and MeadtR for CQRS

---
+++ TasksToDo/Mapper/TasksProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TasksToDo.Models;
+using static TasksToDo.ViewModels.Tasks.TasksIndexViewModel;
+
+namespace TasksToDo.Mapper
+{
+    public class TasksProfile : Profile
+    {
+        public TasksProfile()
+        {
+            CreateMap<TaskToDo, TaskListEntry>();
+           
+        }
+    }
+}
---
+++ TasksToDo/Models/TaskToDo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace TasksToDo.Models
+{
+    public class TaskToDo
+    {
+
+        public TaskToDo(string description)
+        {
+            SetDetails(description);
+        }
+        public int UserId { get; set; }
+        public string  Pwd { get; set; }
+
+
+        [Required]
+        [StringLength(500)]
+        public string Description { get; private set; }
+
+        public bool IsComplete { get; private set; }
+
+
+
+        public void SetDetails(string description)
+        {
+            Description = description;
+
+        }
+
+
+        public void MarkComplete()
+        {
+            IsComplete = true;
+        }
+
+        public void MarkIncomplete()
+        {
+            IsComplete = false;
+        }
+    }
+}
